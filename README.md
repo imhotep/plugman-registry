a10a66915d60db8d780f8a11d1a7ac5546ac20b8
Anis Kadri <anis@apache.org>
2013-06-05 19:03:53 -0700

    first test adduser to registry

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
 plugman-registry
 ================
 
-cordova-plugman registry
+This package uses npm and npmjs.org to manage PhoneGap/Cordova plugins.
+
+Available commands
+==================
+
+adduser
+-------
+
+Create a new user on the npm based PhoneGap/Cordova registry
+
+publish
+-------
+
+Publish a new PhoneGap/Cordova plugin to the npm based PhoneGap/Cordova registry
+
+unpublish
+---------
+
+Unpublish a new PhoneGap/Cordova plugin to the npm based PhoneGap/Cordova registry
+
+Set up a new registry
+=====================
+
+Follow these [steps](https://github.com/isaacs/npmjs.org)
+Make sure you set `secure_rewrites` to `false` in your couchdb configuration.
---
+++ index.js
@@ -0,0 +1,39 @@
+var npm = require('npm'),
+    os = require('os'),
+    rc = require('rc')('plugman', {
+                 registry: 'http://localhost:5984/registry/_design/scratch/_rewrite',
+                 cache: os.tmpdir() + 'plugman',
+                 userconfig: process.env.HOME + '/.plugmanrc'
+               });
+
+function handleError(er) {
+  console.log(er);
+}
+
+npm.on('log', function(message) {
+  console.log(message);
+});
+
+module.exports = {
+  adduser: function(args, cb) {
+    npm.load(rc, function(er) {
+      if (er) return handlError(er);
+      npm.commands.adduser(args, cb);
+    });
+  },
+  publish: function(args, cb) {
+    // TODO read plugin.xml and create package.json
+    // Publish: need to figure out how users are retrieved
+  },
+  unpublish: function(args, cb) {
+  }
+}
+
+function handleError(er) {
+  console.log(er);
+}
+
+module.exports.adduser(null, function(er) {
+  if(er) return handleError(er); 
+  console.log('success');
+});
